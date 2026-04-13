be6b7ebc3f85490a59b28b411989bd8b93d405ae
Queenshafa <queenshafaraniabilqis@gmail.com>
2026-04-13 09:25:38 +0700

    finished the js logic

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
-// elements
 const guessInput = document.getElementById("guessInput");
 const checkBtn = document.getElementById("checkBtn");
 const message = document.getElementById("message");
 const result = document.getElementById("result");
-const actualNum = document.getElementById("actualNumber");
 const attemptsText = document.getElementById("attempts");
 const resetBtn = document.getElementById("resetBtn");
 
 function checkGuess() {
+  const rawValue = guessInput.value;
+  if (rawValue === "") {
+    result.textContent = "WAIT..";
+    message.textContent = "Hey! Put a number first!";
+    return;
+  }
+
   const userGuess = Number(guessInput.value);
+
   attempts++;
   attemptsText.textContent = attempts;
-  if (guess === secretNumber) {
+  if (userGuess === secretNumber) {
     result.textContent = "CORRECT!";
     message.textContent = "Hey! You got it! I think you can read mind!";
-  } else if (guess < secretNumber) {
+    checkBtn.disabled = true;
+  } else if (userGuess < secretNumber) {
     result.textContent = "TOO LOW!";
     message.textContent = "Nope, Try again, higher!";
-  } else if (guess > secretNumber) {
+  } else if (userGuess > secretNumber) {
     result.textContent = "TOO HIGH!";
     message.textContent = "Nope, Try again, lower!";
   }
+
+  guessInput.value = "";
+  guessInput.focus();
 }
 
 checkBtn.addEventListener("click", checkGuess);
